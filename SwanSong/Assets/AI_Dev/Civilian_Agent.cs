52ddb148a118898293c7fc0a1137de8bd54dd979
robotx211 <robert.brede97@gmail.com>
2019-09-03 21:21:59 +0100

    Attempted re-rigging of new swan model

---
+++ SwanSong/Assets/AI_Dev/Civilian_Agent.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Civilian_Agent : MonoBehaviour
+{
+    public enum States
+    {
+        IDLE = 0,
+        FOLLOW = 1,
+        FLEE = 2
+    }
+
+    public States currentState;
+
+    private void Start()
+    {
+
+        currentState = States.IDLE;
+
+    }
+
+    private void Update()
+    {
+
+        switch (currentState)
+        {
+            case States.IDLE:
+                //IDLE
+                Idle_Update();
+                break;
+            case States.FOLLOW:
+                //FOLLOW
+                Follow_Update();
+                break;
+            case States.FLEE:
+                //FLEE
+                Flee_Update();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void Idle_Update()
+    {
+
+    }
+
+    private void Follow_Update()
+    {
+
+    }
+
+    private void Flee_Update()
+    {
+
+    }
+}
