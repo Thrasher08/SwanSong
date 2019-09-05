76d81b6f1db9ede9f2a246da5419816134b2462e
robotx211 <robert.brede97@gmail.com>
2019-09-05 14:27:16 +0100

    New swan model, part of beak seems connected to neck armature

---
+++ SwanSong/Assets/Scripts/EditorPoistionMarker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class EditorPoistionMarker : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(this.transform.position - new Vector3(0.02f, 0, 0), this.transform.position + new Vector3(0.02f, 0, 0), Color.cyan);
+        Debug.DrawLine(this.transform.position - new Vector3(0, 0.02f, 0), this.transform.position + new Vector3(0, 0.02f, 0), Color.cyan);
+        Debug.DrawLine(this.transform.position - new Vector3(0, 0, 0.02f), this.transform.position + new Vector3(0, 0, 0.02f), Color.cyan);
+    }
+}
